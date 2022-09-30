a4d90611407882c97b37981b766d2e28d47d373e
ValerieFH <val.can.code@gmail.com>
2022-09-29 20:30:45 -0500

    added stub routes for basic CRUD functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,51 @@
-//TODO
+
+
+async function getAllPosts(req, res){
+    try {
+        //db call
+        res.send("All Posts endpoint")
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({'message': 'error getting all posts'})
+    }
+}
+
+async function getOnePost(req, res){
+    try {
+        const { id } = req.params
+        //db call
+        res.send("One Posts endpoint for id " + id)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({'message': 'error getting post by ID'})
+    }
+}
+
+async function addPost(req, res){
+    try {
+        // const { id } = req.params
+        //db call
+        res.send("add one post")
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({'message': 'error adding post'})
+    }
+}
+
+async function deletePost(req, res){
+    try {
+        const { id } = req.params
+        //db call
+        res.send("delete one post with id " + id)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({'message': 'error deleting post'})
+    }
+}
+
+module.exports = {
+    getAllPosts,
+    getOnePost,
+    addPost,
+    deletePost
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
+const { getAllPosts, getOnePost, addPost, deletePost } = require('../controllers/index')
 
 router.get('/', (req, res) => {
     res.send("Test Home page")
 })
+
+router.get('/all', getAllPosts)
+router.get('/:id', getOnePost)
+router.post('/', addPost)
+router.delete('/:id', deletePost)
 
 module.exports = router
