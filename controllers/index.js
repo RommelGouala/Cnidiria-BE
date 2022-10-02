438e5a2e8b4353bcc57d49c6501cd3704dad69f7
ValerieFH <val.can.code@gmail.com>
2022-10-02 12:07:42 -0500

    init commit of mongo, added connection data and stubbed out endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,51 +1,9 @@
-//need to correct for live hosting
-const connection = {
-    host: 'localhost',
-    port: 5432,
-    database: 'cnidiria',
-    user: 'postgres',
-    password: ''
-};
-
-// const connection = 'postgres://postgres:postgres@localhost:5432/Cnidiria'
-
-const initOptions = {
-    // Capitalizes all SQL generated
-    capSQL: true,
-    // global event notification;
-    error(error, e) {
-      if (e.cn) {
-        // A connection-related error;
-  
-        // Connections are reported back with the password hashed,
-        // for safe errors logging, without exposing passwords.
-        console.log('CN:', e.cn);
-        console.log('EVENT:', error.message || error);
-      }
-    },
-    // this is to be commited for debugging purposes, but left commented out until you need it
-    // if un-commented, it will print out the resulting query when any query is ran
-    query(e) {
-      console.log(e.query);
-    },
-  };
-
-const pgp = require('pg-promise')(initOptions);
-  
-const db = pgp(connection)
-
-db.connect()
-  .then(obj => {
-    // After query runs succesfully, disconnect
-    obj.done();
-  })
-  .catch(error => {
-    console.log('ERROR FROM DB:========>', error.message);
-  });
+// const Post = require('../models/post')
 
 async function getAllPosts(req, res){
     try {
-        //db call table is cUsers
+        // const posts = await Post.find()
+        //res.send(posts)
         res.send("All Posts endpoint")
     } catch (error) {
         console.log(error)
@@ -56,7 +14,8 @@ async function getAllPosts(req, res){
 async function getOnePost(req, res){
     try {
         const { id } = req.params
-        //db call
+        //const post = await Post.findById(id)
+        //res.send(post)
         res.send("One Posts endpoint for id " + id)
     } catch (error) {
         console.log(error)
@@ -66,36 +25,13 @@ async function getOnePost(req, res){
 
 async function addPost(req, res){
     try {
-        info = ['Test Title', "test test test", "2022-10-02", 30, "madison", 3000, "remote", "Bob" ]
-        const data = await db.one(
-            `INSERT
-             INTO
-              cUsers (
-                title, 
-                desc, 
-                date, 
-                timeframe, 
-                location, 
-                budget, 
-                jobtype, 
-                postowner
-                ) 
-                VALUES (
-                    $/title/, 
-                    $/desc/, 
-                    $/date/, 
-                    $/timeframe/, 
-                    $/location/, 
-                    $/budget/, 
-                    $/jobtype/, 
-                    $/postowner/
-                    ) 
-                    RETURNING id`, 
-                    info
-                    )
-            console.log(data)
+        // const { title, desc, date, timeframe, location, jobtype, budget, postowner } = req.body
+        // const post = await new Post({
+        //     ...req.body
+        // }).save()
 
-        res.send("add one post with id " + data)
+        // res.status(201).json({ 'message': 'Post successfully created'})
+        res.send("added one post")
     } catch (error) {
         console.log(error)
         res.status(500).json({'message': 'error adding post'})
@@ -105,7 +41,8 @@ async function addPost(req, res){
 async function deletePost(req, res){
     try {
         const { id } = req.params
-        //db call
+        // const post = await Post.findByIdAndDelete(id)
+        // res.json({"message": `Post id ${id} had been deleted.`})
         res.send("delete one post with id " + id)
     } catch (error) {
         console.log(error)
