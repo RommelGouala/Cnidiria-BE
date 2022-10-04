3412ab59c6d141fc7d3c8639f859404444d7c72d
hansnielsen8 <hk32077@gmail.com>
2022-10-04 18:44:45 -0500

    Connected posts to users

--- models/post.js
+++ models/post.js
@@ -28,8 +28,9 @@ const postSchema = new mongoose.Schema({
         type: Number
     },
     postOwner: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
     }
 
 })
